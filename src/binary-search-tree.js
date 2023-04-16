176969ba7bdb08354e4f012f0821afa16eb8c17c
Ilya <19962191a@gmail.com>
2023-04-16 19:29:00 +0300

    feat: add tree add, find, min, max, root

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,31 +1,87 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
+
+
+
 class BinarySearchTree {
 
+  constructor() {
+    this.r = null
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+          return this.r
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    const newNode = new Node(data)
+    if (!this.r) {
+      this.r = newNode
+      return
+    }
+
+    let currentNode = this.r
+
+    while(currentNode) {
+      if(newNode.data < currentNode.data){
+        if(!currentNode.left) {
+          currentNode.left = newNode
+          return
+        }
+        currentNode = currentNode.left
+      } else {
+        if(!currentNode.right) {
+          currentNode.right = newNode
+          return
+        }
+        currentNode = currentNode.right
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    const queue = [this.r]
+    while(queue.length){
+      const node = queue.shift()
+      
+
+      if(node.left){
+        queue.push(node.left)
+      }
+
+      if(node.right){
+        queue.push(node.right)
+      }
+      if(data == node.data) {
+        return true
+      }
+    }
+    
+    return false
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    const queue = [this.r]
+    while(queue.length){
+      const node = queue.shift()
+      if(data == node.data) {
+        return node
+      }
+      if(node.left){
+        queue.push(node.left)
+      }
+      if(node.right){
+        queue.push(node.right)
+      }
+    }  
+    return null
   }
 
   remove(/* data */) {
@@ -34,13 +90,27 @@ class BinarySearchTree {
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const queue = [this.r]
+    while(queue.length){
+      const node = queue.shift()
+      if(node.left){
+        queue.push(node.left)
+      } else {
+        return node.data
+      }
+    }  
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const queue = [this.r]
+    while(queue.length){
+      const node = queue.shift()
+      if(node.right){
+        queue.push(node.right)
+      } else {
+        return node.data
+      }
+    }  
   }
 }
 
