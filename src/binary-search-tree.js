d60b8b030981380a9c31433d058b1326da5c4c47
Ilya <19962191a@gmail.com>
2023-04-16 22:48:17 +0300

    finish bt

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -68,25 +68,108 @@ class BinarySearchTree {
   }
 
   find(data) {
-    const queue = [this.r]
-    while(queue.length){
-      const node = queue.shift()
-      if(data == node.data) {
-        return node
+    
+    function recFind(currentNode){
+        if(currentNode == null){
+            return null
+        } else if(currentNode.data === data){
+            return currentNode
+        } else if(currentNode.data > data){
+            return recFind(currentNode.left)
+        } else {
+            return recFind(currentNode.right)
+        }
+    } 
+    return recFind(this.r)
+
+}  
+
+  remove(data) {
+    function recFindDel(currentNode, prevNode){
+      if(currentNode == null){
+          return null
+      } else if(currentNode.data === data){
+          return {node: currentNode, prev: prevNode}
+      } else if(currentNode.data > data){
+          return recFindDel(currentNode.left, currentNode)
+      } else {
+          return recFindDel(currentNode.right, currentNode)
       }
-      if(node.left){
-        queue.push(node.left)
+  } 
+
+  function deleteWOChild(node, prev){
+      if(!prev){
+          this.r = null
+      } else {
+          if(prev.left == node){
+              prev.left = null
+          } else {
+              prev.right = null
+          }
       }
-      if(node.right){
-        queue.push(node.right)
-      }
-    }  
-    return null
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  function deleteOneChild(node, prev){
+      let child
+      if(node.left){
+          child = node.left
+      } else {
+          child = node.right
+      }
+      if(!prev) {
+          this.r = child
+      } else {
+          if(prev.left == node){
+              prev.left = child
+          } else {
+              prev.right = child
+          }
+      }
+  }
+
+  function deleteTwoChild(node, prev){
+      let prevChild = node
+      let child = node.left
+      if(child.right){
+          while(child.right){
+              prevChild = child
+              child = child.right
+          }
+      }
+
+      deleteOneChild.call(this, child)
+
+      let leftChild = node.left
+      let rightChild = node.right
+
+      if(!prev){
+          child.left = leftChild
+          child.right = rightChild
+          this.r = child
+      } else {
+          if(prev.left == node){
+              child.left = leftChild
+              child.right = rightChild
+              prev.left = child
+          } else {
+              child.left = leftChild
+              child.right = rightChild
+              prev.left = child
+          }
+      }
+  }
+
+  let del = recFindDel(this.r, null)
+  let node = del.node
+  let prev = del.prev
+
+  if(!node.left && !node.right){
+      deleteWOChild.call(this, node, prev)
+  } else if (!node.left && node.right || node.left && !node.right){
+      deleteOneChild.call(this, node, prev)
+  } else {
+      deleteTwoChild.call(this, node, prev)
+  }
   }
 
   min() {
